93dadfd8292718ef94a596530920d6cea2e0c2e2
Anemoiaa <georgiypetroff@gmail.com>
2026-01-08 03:31:24 +0400

    feat: implement home page

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -3,11 +3,58 @@
 namespace Anemoiaa\AhTestAssignment\controllers;
 
 use Anemoiaa\AhTestAssignment\core\Controller;
+use Anemoiaa\AhTestAssignment\repositories\CategoryRepository;
+use Smarty\Smarty;
 
 class HomeController extends Controller
 {
+    private CategoryRepository $categoryRepo;
+
+    public function __construct(Smarty $view)
+    {
+        parent::__construct($view);
+        $this->categoryRepo = new CategoryRepository();
+    }
+
     public function index(): void
     {
-        $this->view->display('home.tpl');
+        $template = 'home.tpl';
+        $cacheLifetime = 300;
+
+        $this->view->setCaching(Smarty::CACHING_LIFETIME_CURRENT);
+        $this->view->setCacheLifetime($cacheLifetime);
+
+        if ($this->view->isCached($template)) {
+            $this->render($template);
+            return;
+        }
+
+        $data = $this->categoryRepo->withLastPosts(3);
+        $grouped = [];
+
+        foreach ($data as $item) {
+            $categoryName = $item['category_name'];
+
+            if (!isset($grouped[$categoryName])) {
+                $grouped[$categoryName] = [
+                    'id'    => $item['category_id'],
+                    'name'  => $item['category_name'],
+                    'posts' => [],
+                ];
+            }
+
+            $grouped[$categoryName]['posts'][] = [
+                'id'          => $item['post_id'],
+                'title'       => $item['title'],
+                'description' => $item['description'],
+                'image'       => $item['image'],
+                'views'       => $item['views'],
+                'created_at'  => $item['created_at'],
+            ];
+        }
+
+        $this->render($template, [
+            'categories' => $grouped
+        ]);
     }
 }
--- resources/views/home.tpl
+++ resources/views/home.tpl
@@ -1,1 +1,22 @@
-Hello
+{extends file="layouts/main.tpl"}
+
+{block name="content"}
+    {foreach $categories as $category}
+        <section class="category-preview container">
+            <div class="category-preview__header">
+                <h2 class="category-preview__title title-2">
+                    {$category['name']}
+                </h2>
+                <a href="/categories/{$category['id']}" class="category-preview__link">
+                    View All
+                </a>
+            </div>
+
+                <div class="category-preview__list grid-cols-3">
+                {foreach $category['posts'] as $post}
+                    {include file="post/partials/post-preview.tpl" post=$post}
+                {/foreach}
+            </div>
+        </section>
+    {/foreach}
+{/block}
